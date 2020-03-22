0b17ce81cac30857406d0620eb9020f10109529c
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-03-22 23:33:20 +0000

    Added single player sleep

--- BareNecessities/src/main/java/io/github/laurencerawlings/barenecessities/BareNecessities.java
+++ BareNecessities/src/main/java/io/github/laurencerawlings/barenecessities/BareNecessities.java
@@ -1,5 +1,6 @@
 package io.github.laurencerawlings.barenecessities;
 
+import io.github.laurencerawlings.barenecessities.events.BedListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BareNecessities extends JavaPlugin {
@@ -7,11 +8,15 @@ public final class BareNecessities extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("Registering events...");
+        getServer().getPluginManager().registerEvents(new BedListener(this),
+                this);
+        getLogger().info("Loaded successfully");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Shutting down successfully");
     }
 }
---
+++ BareNecessities/src/main/java/io/github/laurencerawlings/barenecessities/events/BedListener.java
@@ -0,0 +1,50 @@
+package io.github.laurencerawlings.barenecessities.events;
+
+import io.github.laurencerawlings.barenecessities.BareNecessities;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+
+import java.util.Random;
+
+public class BedListener implements Listener {
+    public static long WakeUp = 0;
+    public static long Delay = 5;
+    public static boolean RandomMessages = true;
+    public static ChatColor MessageColor = ChatColor.YELLOW;
+
+    public static String[] Messages = new String[]{
+            "Ssh, %s is sleeping...",
+            "%s has gone to bed."
+    };
+
+    BareNecessities plugin;
+
+    public BedListener(BareNecessities plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onEnterBed(PlayerBedEnterEvent event) {
+        Player player = event.getPlayer();
+
+        if (event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
+            String message = String.format(getMessage(),
+                    event.getPlayer().getDisplayName());
+            plugin.getServer().broadcastMessage(message);
+            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin,
+                    () -> player.getWorld().setTime(WakeUp),
+                    Delay * 20L);
+        }
+    }
+
+    private String getMessage() {
+        int i = 0;
+        if (RandomMessages) {
+            i = new Random().nextInt(Messages.length);
+        }
+        return MessageColor + Messages[i];
+    }
+}
